027acb43e33a02eae93fab582e99c938c4678814
szentedaniel <karifull2000@gmail.com>
2021-02-24 10:54:44 +0100

    ejj

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>Kitchen Ninja</div>
+        <div>Kitchen Ninjawsdasdasd</div>
         <img src={logo} alt=""/>
       </header>
     </div>

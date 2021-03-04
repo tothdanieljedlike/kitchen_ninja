8b22277e2e07b60de7ffbc591310b41bf3a29362
szentedaniel <karifull2000@gmail.com>
2021-03-04 23:45:42 +0100

    a

--- src/Ball.js
+++ src/Ball.js
@@ -2,15 +2,9 @@ import React from 'react';
 
 function Ball(props) {
 
-    let x = null;
-    let y = null;
-         
-    x = window.innerWidth/2;
-    y = window.innerHeight/2;
 
-
-    x =+ 100 + props.Accelerometer_x*5;
-    y =+ 100 + props.Accelerometer_y*5;
+    let x =+  props.Accelerometer_x*5;
+    let y =+  props.Accelerometer_y*5;
         
 
 

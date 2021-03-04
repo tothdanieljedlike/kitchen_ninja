9c2cc224ba95e7c692ae1644935ac131ba1b5347
szentedaniel <karifull2000@gmail.com>
2021-03-05 00:15:28 +0100

    🤐🤐

--- src/Ball.js
+++ src/Ball.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Ball(props) {
+    //const [test, settest] = useState(0)
+    const [cord, setcord] = useState({x: 0, y: 0})
 
+    let x = cord.x;
+    let y = cord.y;
 
-    let x =+  props.Accelerometer_x*5;
-    let y =+  props.Accelerometer_y*5;
-        
+    useEffect(() => {
+        const handleMotionEvent = event => {
+            let a = cord.x;
+            let b = cord.y;
+            a += props.Accelerometer_x*5;
+            b += props.Accelerometer_y*5;
+            setcord({x: a, y: b});
+        };
+        window.addEventListener('devicemotion', handleMotionEvent, true);
+        return () => window.removeEventListener('devicemotion', handleMotionEvent);
+    }, []);
+    
+    
+    // const inputTextHandler = (e) => {
+    //     console.log(parseInt(e.target.value));
+    //     settest(parseInt(e.target.value));
+    // }
 
+    // const submitHandler = (e) => {
+    //     e.preventDefault();
+    //     console.log(test + ", " + cord.x + ", " + cord.y);
+    //     let a = cord.x;
+    //     let b = cord.y;
+    //     a += test*5;
+    //     b += test*5;
+    //     setcord({x: a, y: b});
+    // }
 
     const ballStyle = {
         top: y,
@@ -16,6 +43,10 @@ function Ball(props) {
 
     return(
         <div>
+            <form >
+                {/* <input onChange={inputTextHandler} type="text"/>
+                <button onClick={submitHandler} type="submit"></button> */}
+            </form>
             <div className="ball" style={ballStyle}></div>
         </div>
     );

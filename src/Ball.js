21fafdfda4eac91126639f520f1e4e6977293217
szentedaniel <karifull2000@gmail.com>
2021-03-05 00:41:17 +0100

    😀

--- src/Ball.js
+++ src/Ball.js
@@ -1,39 +1,41 @@
 import React, {useState, useEffect} from 'react';
 
 function Ball(props) {
-    //const [test, settest] = useState(0)
+    // const [test, settest] = useState(0)
     const [cord, setcord] = useState({x: 0, y: 0})
 
     let x = cord.x;
     let y = cord.y;
 
-    useEffect(() => {
+     useEffect(() => {
+        let a = cord.x;
+        let b = cord.y;
         const handleMotionEvent = event => {
-            let a = cord.x;
-            let b = cord.y;
-            a += event.acceleration.x*5;
-            b += event.acceleration.y*5;
-            setcord({x: a, y: b});
-        };
-        window.addEventListener('devicemotion', handleMotionEvent, true);
-        return () => window.removeEventListener('devicemotion', handleMotionEvent);
-    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+             
+             a += parseInt(event.acceleration.x*5);
+             b += parseInt(event.acceleration.y*5);
+             setcord({x: a, y: b});
+         };
+         
+         window.addEventListener('devicemotion', handleMotionEvent, true);
+         return () => window.removeEventListener('devicemotion', handleMotionEvent);
+     }, []); // eslint-disable-line react-hooks/exhaustive-deps
     
     
-    // const inputTextHandler = (e) => {
-    //     console.log(parseInt(e.target.value));
-    //     settest(parseInt(e.target.value));
-    // }
+    //  const inputTextHandler = (e) => {
+    //      console.log(parseInt(e.target.value));
+    //      settest(parseInt(e.target.value));
+    //  }
 
-    // const submitHandler = (e) => {
-    //     e.preventDefault();
-    //     console.log(test + ", " + cord.x + ", " + cord.y);
-    //     let a = cord.x;
-    //     let b = cord.y;
-    //     a += test*5;
-    //     b += test*5;
-    //     setcord({x: a, y: b});
-    // }
+    //  const submitHandler = (e) => {
+    //      e.preventDefault();
+    //      console.log(test + ", " + cord.x + ", " + cord.y);
+    //      let a = cord.x;
+    //      let b = cord.y;
+    //      a += test*5;
+    //      b += test*5;
+    //      setcord({x: a, y: b});
+    //  }
 
     const ballStyle = {
         top: y,
@@ -46,7 +48,7 @@ function Ball(props) {
             <div>x: {cord.x}, y: {cord.y}</div>
             <form >
                 {/* <input onChange={inputTextHandler} type="text"/>
-                <button onClick={submitHandler} type="submit"></button> */}
+                <button onClick={submitHandler} type="submit">a</button>  */}
             </form>
             <div className="ball" style={ballStyle}></div>
         </div>
